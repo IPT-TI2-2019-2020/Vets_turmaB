7e5b8ffccaa55db58e1a28387dfa51d3369c4310
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2020-04-06 11:42:20 +0100

    Adição da classe que representa a BD

--- Vets/Vets/Startup.cs
+++ Vets/Vets/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Vets.Data;
 
 namespace Vets {
    public class Startup {
@@ -20,6 +22,13 @@ namespace Vets {
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services) {
          services.AddControllersWithViews();
+
+         //****************************************************************************
+         // especificação do 'tipo' e 'localização' da BD
+         services.AddDbContext<VetsDB>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ConnectionDB")));
+         //****************************************************************************
+
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
